4ea00d6909c94d30c6457693372fc620bd4922cf
nim3xh <105283769+nim3xh@users.noreply.github.com>
2022-05-10 16:52:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function findBMI() {
+    var weight = document.getElementById("weight").value;
+    var height = document.getElementById("height").value;
+  
+    var Bmi = weight / (height * height);
+    var State;
+  
+    if (Bmi < 60 && Bmi > 29) {
+      State = "Obese";
+      $(document).ready(function () {
+        $("#obese").fadeIn("slow");
+        $("#over").hide();
+        $("#normal").hide();
+      });
+    } else if (Bmi <= 29 && Bmi > 24) {
+      State = "Over weight";
+      $(document).ready(function () {
+        $("#over").fadeIn("slow");
+        $("#normal").hide();
+        $("#obese").hide();
+      });
+    } else if (Bmi <= 24 && Bmi >= 19) {
+      State = "Normal";
+      $(document).ready(function () {
+        $("#normal").fadeIn("slow");
+        $("#over").hide();
+        $("#obese").hide();
+      });
+    }
+    $(document).ready(function () {
+      $(".output").fadeIn();
+    });
+  
+    document.getElementById("bmiValue").innerHTML = Math.round(Bmi * 100) / 100;
+    document.getElementById("state").innerHTML = State;
+  }
+  
+  $(document).ready(function () {
+    $("#reset").click(function () {
+      $(".output").fadeOut();
+      $("img").hide("slow");
+    });
+  });
